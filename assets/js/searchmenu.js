36507801f97066d0b9aae625d028337e5d8e4b3a
mintaeKim <peterzzo@naver.com>
2024-03-01 03:04:41 +0900

    240301 date update auto add

--- assets/js/searchmenu.js
+++ assets/js/searchmenu.js
@@ -1,9 +1,23 @@
 function gotopage() {
     const menuname = document.getElementById('menuinput').value;
     if(menuname === "꿀백설기"){
-        window.location.href=("properties.html");
+        window.location.href=("index.html");
+        return;
     }
-    if(menuname === "호박떡"){
-        window.location.href=("property-details.html");
+    if(menuname === "단호박편"){
+        window.location.href=("index.html");
+        return;
+    }
+    else if(menuname.length === 0){
+        alert("검색어를 입력해 주세요.");
+    }
+    else {
+        alert(menuname + "에 대한 검색결과가 없습니다.");
     }
 }
+
+const myyear = document.getElementById("currentyear");
+const now = new Date();
+const year = now.getFullYear();
+const since = year - 1995;
+myyear.innerText = since;
